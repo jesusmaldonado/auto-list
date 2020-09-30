ca2cb9eaad0f06ebd34b5f2b83e8f579875f97e3
jesusmaldonado <jesusmaldonado317@gmail.com>
2020-09-30 12:05:35 +0200

    adds basic MUI styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Button from "@material-ui/core/Button";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#4A4A4A",
+    },
+    secondary: {
+      main: "#EDEDED",
+    },
+  },
+  overrides: {
+    MuiButton: {
+      containedPrimary: {
+        backgroundColor: "#EA7F2B",
+        width: "128px",
+        height: "32px",
+        "&:focus": {
+          backgroundColor: "#D37324",
+        },
+      },
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Button variant="contained" color="primary">
+        Save
+      </Button>
+    </ThemeProvider>
   );
 }
 
