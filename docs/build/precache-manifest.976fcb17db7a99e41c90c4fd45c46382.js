7ed54c5e2543c80695f9610a87cd93a74e102cc1
jesusmaldonado <jesusmaldonado317@gmail.com>
2020-10-22 09:23:21 +0200

    moves build to docs

---
+++ docs/build/precache-manifest.976fcb17db7a99e41c90c4fd45c46382.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b39d73a92481503182a0ac658087a0ca",
+    "url": "/auto-list/index.html"
+  },
+  {
+    "revision": "b8bac5c9f7c2ba585906",
+    "url": "/auto-list/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "8d1b32e6d7855fe38657",
+    "url": "/auto-list/static/js/2.376af81b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/auto-list/static/js/2.376af81b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8bac5c9f7c2ba585906",
+    "url": "/auto-list/static/js/main.b73a84df.chunk.js"
+  },
+  {
+    "revision": "b2e0012f0c610191c7ac",
+    "url": "/auto-list/static/js/runtime-main.77bbb50b.js"
+  },
+  {
+    "revision": "b4440b18387046dca7ceb9a053653a6d",
+    "url": "/auto-list/static/media/autologo.b4440b18.jpg"
+  }
+]);
